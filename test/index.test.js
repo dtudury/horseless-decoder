91f5edaef17fc7eeed0f75e83dd161d94dc71e2f
David Tudury <david.tudury@gmail.com>
2020-03-03 10:23:35 -0800

    test html escaped strings

--- test/index.test.js
+++ test/index.test.js
@@ -6,6 +6,11 @@ import { h } from '../index.js'
 describe('h', function () {
   it('should parse basic html-like strings', function () {
     const decoded = h`<x a="b">c</x>`
-    console.log(decoded)
+    console.log(JSON.stringify(decoded))
+  })
+  it('should parse html escaped strings', function () {
+    const decoded = h`&amp;&apos;&gt;&lt;&quot;`
+    assert.deepEqual(decoded, [{ type: 'textnode', value: '&\'><"' }])
+    assert.throws(function () { h`&nope;` })
   })
 })
