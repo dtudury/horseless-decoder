db6329890cebc4b51eb6db1ed232534e9099a5d2
David Tudury <david.tudury@gmail.com>
2020-03-04 11:31:14 -0800

    basic code coverage test complete

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,7 @@
 
 /* global describe it */
 import { assert } from 'chai'
-import { h } from '../index.js'
+import { h, FRAGMENT } from '../index.js'
 
 describe('h', function () {
   describe('basic decoding', function () {
@@ -25,14 +25,34 @@ describe('h', function () {
       assert.equal(decoded[1].attributes.b, '&\'><"')
       assert.throws(function () { h`&nope;` })
     })
-    it('whould parse elements with values', function () {
+    it('should parse elements with values for tags and attributes', function () {
       const decoded = h`<${'a'} ${{ b: 'c' }} />`
       assert.equal(decoded[0].tag, 'a')
       assert.deepEqual(decoded[0].attributes, { b: 'c' })
     })
-    it('should parse attributes with values as arrays of strings interpolated with values', function () {
-      const decoded = h`<a b="${1} 2 ${3}"/>`
+    it('should parse attributes with interpolated values', function () {
+      const decoded = h`<a b="${1} 2 ${3}" c=${4}/>`
       assert.deepEqual(decoded[0].attributes.b, [1, ' 2 ', 3])
+      assert.equal(decoded[0].attributes.c, 4)
     })
+    it('should parse values as elements', function () {
+      const decoded = h`${1}<a>${h`<b>c</b>`}</a>${null}`
+      assert.equal(decoded[1].children[0][0].children[0].value, 'c')
+      assert.equal(decoded.length, 2)
+    })
+    it('should parse fragment nodes', function () {
+      const decoded = h`<>a</>`
+      assert.equal(decoded[0].tag, FRAGMENT)
+      assert.equal(decoded[0].children[0].value, 'a')
+    })
+    it('should handle different namespaces', function () {
+      assert.equal(h`<html/>`[0].xmlns, 'http://www.w3.org/1999/xhtml')
+      assert.equal(h('http://www.w3.org/2000/svg')`<svg/>`[0].xmlns, 'http://www.w3.org/2000/svg')
+    })
+  })
+  describe('html special decoding', function () {
+    //it('should handle void elements')
+    //it('should handle boolean attributes')
+    //it('should handle unquoted attributes')
   })
 })
